0f6f65dcffbba209dd5f5dd4a1fc92d879ca2c72
felipemedeiroszz <felipemedeirosclay@icloud.com>
2026-06-18 13:37:08 -0300

    responsive

--- src/components/Process.tsx
+++ src/components/Process.tsx
@@ -53,7 +53,7 @@ export default function Process() {
           </p>
         </motion.div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-8">
           {cards.map((card, index) => (
             <motion.div
               key={index}
--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -57,7 +57,7 @@ export default function Products() {
           </p>
         </motion.div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-8">
           {cards.map((card, index) => (
             <motion.div
               key={index}
